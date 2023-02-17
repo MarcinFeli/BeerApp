ab8db2b9c5b98c6126e1b9715c0d77920ebff812
Marcin <marcinfelinczak123@gmail.com>
2023-02-17 08:43:24 +0100

    navbar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,40 @@
 <template>
-  <HelloWorld />
+	<v-app>
+    <v-card class="mx-auto" color="grey-lighten-3" max-width="400">
+      <v-card-text>
+        <v-text-field
+         
+          density="compact"
+          variant="solo"
+          label="Search templates"
+          append-inner-icon="mdi-magnify"
+          single-line
+          hide-details
+          @click:append-inner="onClick"></v-text-field>
+      </v-card-text>
+    </v-card>
+  </v-app>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+export default {
+	data: () => ({
+		
+	}),
 
-  export default {
-    name: 'HomeView',
+	methods: {
+		onClick() {
+			this.loading = true
 
-    components: {
-      HelloWorld,
-    },
-  }
+			setTimeout(() => {
+				this.loading = false
+				this.loaded = true
+			}, 2000)
+		},
+	},
+}
+// import { computed } from 'vue'
+// import store from '@/store'
+
+// const beers = computed(() => store.state.beers)
 </script>
